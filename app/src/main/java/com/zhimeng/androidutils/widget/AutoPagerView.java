8e493653b9137177ae19224512bfcbc7655d2ee3
Rong xianzhuo <rongxz@zhongbosoft.com>
2018-03-12 15:37:49 +0800

    add AutoPagerView

--- app/src/main/java/com/zhimeng/androidutils/widget/AutoPagerView.java
+++ app/src/main/java/com/zhimeng/androidutils/widget/AutoPagerView.java
@@ -1,8 +1,42 @@
 package com.zhimeng.androidutils.widget;
 
-/**
- * Created by Administrator on 2018/3/12 0012.
- */
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.AttributeSet;
 
-public class AutoPagerView {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class AutoPagerView extends SimplePagerView {
+
+    private Timer timer;
+    private Handler handler = new Handler(Looper.getMainLooper());
+
+    public AutoPagerView(Context context) {
+        super(context);
+    }
+
+    public AutoPagerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setup(final int delay, int period, SimplePagerView.PageItem... items) {
+        super.setup(items);
+        if (timer != null) return;
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (isFingerTouching()) return;
+                if (System.currentTimeMillis() - getLastFlingTime() < delay) return;
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        turnForward(1);
+                    }
+                });
+            }
+        }, delay, period);
+    }
 }
