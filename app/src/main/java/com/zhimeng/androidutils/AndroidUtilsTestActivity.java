b9009cca1b0fe1c90e77869e7e7362959a3f6942
Rong xianzhuo <rongxz@zhongbosoft.com>
2018-03-23 15:47:34 +0800

    Add PixelBuffer (opengl off-screen render)

--- app/src/main/java/com/zhimeng/androidutils/AndroidUtilsTestActivity.java
+++ app/src/main/java/com/zhimeng/androidutils/AndroidUtilsTestActivity.java
@@ -1,12 +1,16 @@
 package com.zhimeng.androidutils;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.zhimeng.androidutils.opengl.PixelBuffer;
 import com.zhimeng.androidutils.opengl.demo.GLDemoRenderer;
 import com.zhimeng.androidutils.opengl.demo.GLDemoView;
 import com.zhimeng.androidutils.widget.SimplePagerView;
@@ -17,5 +21,10 @@ public class AndroidUtilsTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_utils_test);
+        PixelBuffer buffer = new PixelBuffer(500, 500);
+        buffer.setRenderer(new GLDemoRenderer(BitmapFactory.decodeResource(getResources(), R.drawable.test)));
+        Bitmap bitmap = buffer.getBitmap();
+        buffer.destroy();
+        ((ImageView) findViewById(R.id.image_view)).setImageBitmap(bitmap);
     }
 }
